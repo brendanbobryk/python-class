9a7d2fc0160a2fd968f0806bc38257d9dac01b03
Brendan Bobryk <babobryk@gmail.com>
2026-04-13 22:25:20 -0700

    Revert "added week 120 assignments"
    This reverts commit 6c0b82040d5154693b194826b047309069346da4.

--- week_10/lab.py
+++
@@ -1,6 +0,0 @@
-print("Excercise 1 & 2:\n---------------")
-with open("lab_file.txt", "w") as file:
-    file.write("Hello, world!")
-
-with open("lab_file.txt", "r") as file:
-    print(file.read())
