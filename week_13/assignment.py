fa3cb2f1b961b49c88899cb51badc813f0e54550
Brendan Bobryk <babobryk@gmail.com>
2026-04-27 21:41:55 -0700

    added comments

--- week_13/assignment.py
+++ week_13/assignment.py
@@ -3,23 +3,23 @@ from abc import ABC, abstractmethod
 # Abstract class
 class Shape(ABC):
     def __init__(self, c):
-        self.colour = c
+        self.colour = c     # assigns value of c to colour attribute
     
     def get_colour(self):
-        return self.colour
+        return self.colour  # returns value of colour
     
     @abstractmethod
-    def get_area(self):
+    def get_area(self):     # abstract method
         pass
 
 # Derived class
 class Square(Shape):
     def __init__(self, c, side):
-        super().__init__(c)
-        self.side = side
+        super().__init__(c)         # calls super(c) to initialize the color
+        self.side = side            # assigns the value to side.
     
     def get_area(self):
-        return self.side * self.side
+        return self.side * self.side    # returns the area of the square
     
 colour = "blue"
 side = 7.0
