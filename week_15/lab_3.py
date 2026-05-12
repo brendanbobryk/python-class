a6f2e30ec1372906d5be4f0e17e000cf398358a6
Brendan Bobryk <babobryk@gmail.com>
2026-05-11 20:33:04 -0700

    added test input and display

--- week_15/lab_3.py
+++ week_15/lab_3.py
@@ -4,7 +4,14 @@
 def sum_list(numbers):
     total = 0
 
-    for number in numbers:
+    for number in numbers:  # Loop through each number in the list and add it to total
         total += number
 
     return total
+
+my_list = [10, 20, 30, 40, 50]
+
+result = sum_list(my_list)
+
+print("The list is:", my_list)
+print("The sum of all items in the list is:", result)
