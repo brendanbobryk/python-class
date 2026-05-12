1dd2b004280ba060f2b32322ca0a92f878fb2881
Brendan Bobryk <babobryk@gmail.com>
2026-05-11 20:21:58 -0700

    constructed recursive function

--- week_15/lab_2.py
+++ week_15/lab_2.py
@@ -1,2 +1,16 @@
 # Question 2:
 # Write a Python program to calculate the sum of a list of numbers using recursion.
+
+def recursive_sum(data):
+    total = 0
+
+    # Go through each item in the list
+    for item in data:
+        # If the item is itself a list, recursively sum it
+        if isinstance(item, list):
+            total += recursive_sum(item)
+        else:
+            # Otherwise, add the number directly
+            total += item
+
+    return total
