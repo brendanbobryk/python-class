f841b49a167840487a74f41422ab992edd2387a7
Brendan Bobryk <babobryk@gmail.com>
2026-05-11 20:17:38 -0700

    constructed maximum function

--- week_15/lab_1.py
+++ week_15/lab_1.py
@@ -1,2 +1,10 @@
 # Question 1:
 # Write a Python function to find the maximum of three numbers.
+
+def maximum(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
