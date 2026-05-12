f4849f3730864b9353b387670d8477a5f29e1c16
Brendan Bobryk <babobryk@gmail.com>
2026-05-11 20:22:59 -0700

    added test inputs and display

--- week_15/lab_2.py
+++ week_15/lab_2.py
@@ -14,3 +14,15 @@ def recursive_sum(data):
             total += item
 
     return total
+
+list1 = [1, 2, [3]]
+print("Input:", list1)
+print("Output:", recursive_sum(list1))
+
+list2 = [[4, 5], [7, 8, [20]], 100]
+print("\nInput:", list2)
+print("Output:", recursive_sum(list2))
+
+list3 = [[1, 2, 3], [4, [5, 6]], 7]
+print("\nInput:", list3)
+print("Output:", recursive_sum(list3))
