b71f36d8f47e8f336cd3b8e8e94ba8abdd638bf5
Brendan Bobryk <babobryk@gmail.com>
2026-05-11 20:32:03 -0700

    constructed function to sum items in a list

--- week_15/lab_3.py
+++ week_15/lab_3.py
@@ -1,2 +1,10 @@
 # Question 3:
 # Write a Python program to sum all the items in a list.
+
+def sum_list(numbers):
+    total = 0
+
+    for number in numbers:
+        total += number
+
+    return total
