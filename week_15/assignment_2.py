3e7de3dbfc6b99ee559a7baca520ae919421709d
Brendan Bobryk <babobryk@gmail.com>
2026-05-12 23:08:40 -0700

    fixed comment alignment in question 2

--- week_15/assignment_2.py
+++ week_15/assignment_2.py
@@ -1,2 +1,9 @@
 # Question 2:
-# Define a ShoppingCart class representing a shopping cart. It has an attribute called items, which is initially an empty list.
+# Define a ShoppingCart class representing a shopping cart. It has an attribute called 
+#   items, which is initially an empty list.
+# The "add_item()" method takes an item name and quantity as arguments and adds them as 
+#   a tuple to the items list. This adds the item to the cart.
+# The "remove_item()" method removes the first occurrence of an item with the specified 
+#   name from the items list. It iterates over the list and checks if the current item's 
+#   name matches the specified name. If a match is found, it removes the item from the 
+#   list and breaks out of the loop.
