85f2f8c5026c321561a9952f2c2726d87a257b49
Brendan Bobryk <babobryk@gmail.com>
2026-05-12 23:01:08 -0700

    created subsets class

--- week_15/assignment_1.py
+++ week_15/assignment_1.py
@@ -2,3 +2,17 @@
 # Write a Python class to get all possible unique subsets from a set of distinct integers.
 # Input : [4, 5, 6]
 # Output : [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
+
+class Subsets:
+    def get_subsets(self, numbers):
+        result = [[]]
+
+        for number in numbers:
+            new_subsets = []
+
+            for subset in result:
+                new_subsets.append(subset + [number])
+
+            result.extend(new_subsets)
+
+        return result
