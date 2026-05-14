2a3acf5026be144dedee0604ac66dd27835be0ea
Brendan Bobryk <babobryk@gmail.com>
2026-05-13 20:01:10 -0700

    implemented recursive function

--- week_15/assignment_4.py
+++ week_15/assignment_4.py
@@ -1,3 +1,9 @@
 # Question 4:
 # Write a Python program to find the greatest common divisor (GCD) of two integers using 
 #   recursion.
+
+def gcd(a, b):
+    if b == 0:
+        return a
+
+    return gcd(b, a % b)
