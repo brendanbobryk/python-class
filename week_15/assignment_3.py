36e172164c6c4e108efd7c26988a70ecd615f0c8
Brendan Bobryk <babobryk@gmail.com>
2026-05-14 19:08:00 -0700

    created list of square numbers from 1 to 30

--- week_15/assignment_3.py
+++ week_15/assignment_3.py
@@ -1,3 +1,5 @@
 # Question 3:
 # Write a Python program to generate and print a list of the first and last 5 elements 
 #   where the values are square numbers between 1 and 30 (both included).
+
+squares = [x ** 2 for x in range(1, 31) if 1 <= x ** 2 <= 30]
