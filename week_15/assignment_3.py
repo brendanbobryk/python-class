261265bf948126806766bafb5978e4527bd0702c
Brendan Bobryk <babobryk@gmail.com>
2026-05-14 19:09:33 -0700

    printing both results

--- week_15/assignment_3.py
+++ week_15/assignment_3.py
@@ -7,3 +7,9 @@ squares = [x ** 2 for x in range(1, 31) if 1 <= x ** 2 <= 30]
 
 # Combine the first 5 and last 5 elements
 result = squares[:5] + squares[-5:]
+
+print("Square numbers between 1 and 30:")
+print(squares)
+
+print("\nFirst and last 5 elements:")
+print(result)
